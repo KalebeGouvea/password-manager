109b81703ff3a3850a547ad71c8f9387710688b1
KalebeGouvea <k.gouvea@live.com>
2022-01-05 20:28:16 -0300

    pagina principal estruturada com header e footer

---
+++ includes/header.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en" class="h-100">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <title>Gerenciador de senhas</title>
+    </head>
+    <body class="d-flex flex-column h-100">
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Gerenciador de senhas</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        
+                        <ul class="navbar-nav d-inline-flex mt-2 mt-md-0 ms-md-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Sobre</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Olá, Usuário!</a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="#">Sair</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php include 'includes/header.php';?>
+<div class="container">
+    <h1 class="mt-5 mb-4">Senhas salvas</h1>
+    <table class="table table-hover table-bordered">
+        <thead>
+            <tr>
+                <th scope="col">Serviço</th>
+                <th scope="col">Usuário</th>
+                <th scope="col">Senha</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Facebook</td>
+                <td>email@email.com</td>
+                <td>password123</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Google</td>
+                <td>email@email.com</td>
+                <td>password321</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Reddit</td>
+                <td>email@email.com</td>
+                <td>321password</td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<?php include 'includes/footer.php';?>
