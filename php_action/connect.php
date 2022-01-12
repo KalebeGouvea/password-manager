279fbf0d8e8fd444f21992c1546e479bf196ccc1
KalebeGouvea <k.gouvea@live.com>
2022-01-12 20:10:47 -0300

    criada conexao com o DB

---
+++ php_action/connect.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($servername, $username, $password);
+
+if (!$conn) {
+    die("A conexão falhou: " . mysqli_connect_error());
+}
+?>
