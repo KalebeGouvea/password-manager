5787bf7db91ea6245a79fe0c267f0848c67d9071
KalebeGouvea <k.gouvea@live.com>
2022-01-13 20:39:02 -0300

    add links para pagina inicial no header

--- includes/header.php
+++ includes/header.php
@@ -11,13 +11,16 @@
         <header>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Gerenciador de senhas</a>
+                    <a class="navbar-brand" href="index.php">Gerenciador de senhas</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                         
                         <ul class="navbar-nav d-inline-flex mt-2 mt-md-0 ms-md-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.php">Senhas salvas</a>
+                            </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#">Sobre</a>
                             </li>
